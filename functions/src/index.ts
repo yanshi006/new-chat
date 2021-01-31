565efec8b6f1b85332ab96909995f0a9956a5b7f
佐野心哉 <shinya.s.0714@i.softbank.jp>
2021-01-31 22:31:17 +0900

    ブランチができなかったからfirebaseとの連携をやめた

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,28 @@
 // import * as functions from "firebase-functions";
+// import * as admin from "firebase-admin";
+// import { error } from "firebase-functions/lib/logger";
+// //初期化している
+// admin.initializeApp();
+// //省略しているだけ
+// const db = admin.firestore();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// const sendRespons = (respons: functions.Response, statusCode: number, body: any) => {
+//   respons.send({
+//     statusCode,
+//     body: JSON.stringify(body)
+//   });
+// }
+
+// //req: any, res: anyはtypeScriptの書き方
+// export const addDetaset = functions.https.onRequest(async(req: any, res: any) => {
+//   if (req.method !== 'POST') {
+//     sendRespons(res, 405, {error: 'Invalid Request'})
+//   } else {
+//     const detaset = req.body;
+//     for (const key of Object.keys(detaset)) {
+//       const deta = detaset[key];
+//       await db.collection('questions').doc(key).set(deta);
+//     }
+//     sendRespons(res, 200, {message: 'succesfully added detaset'})
+//   }
+// })
